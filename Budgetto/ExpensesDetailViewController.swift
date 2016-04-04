3fd44bda00a451dbdd5f2a7dfe2ad36d1eb2074f
Jens Herlevsen <jensherlevsen@gmail.com>
2016-04-04 15:39:37 +0200

    Lets get it started in here!

---
+++ Budgetto/ExpensesDetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ExpensesDetailViewController.swift
+//  Budgetto
+//
+//  Created by Jens Herlevsen on 04/04/2016.
+//  Copyright © 2016 SJT. All rights reserved.
+//
+
+import UIKit
+
+class ExpensesDetailViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var categoryTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let categoryPicker: UIPickerView = UIPickerView()
+        categoryPicker.delegate = self
+        categoryPicker.dataSource = self
+        
+        categoryTextField.inputView = categoryPicker
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 3
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "Hej"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
