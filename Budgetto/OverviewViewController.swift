5cab8a1af1a800789826cb023d2455a0d56ad554
Jens Herlevsen <jensherlevsen@gmail.com>
2016-04-23 18:20:07 +0200

    Reorganize controllers

--- Budgetto/OverviewViewController.swift
+++ Budgetto/OverviewViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OverviewViewController: UIViewController {
     
     @IBOutlet weak var monthSelectionButton: MonthSelectionButton!
     
